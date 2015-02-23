b025d0bf39389d682d78f35ef6f02a0cbebab5f0
ichi <ichi.ttht.1@gmail.com>
2015-02-24 01:51:48 +0900

    login処理追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_scope :user do
+    # get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
+    get 'sign_in/:provider', :to => redirect('/users/auth/%{provider}'), :as => :sign_in
+    get 'sign_out', :to => 'users/sessions#destroy', :as => :sign_out
+  end
+
   resources :groups
 
   resources :themes do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221161600) do
+ActiveRecord::Schema.define(version: 20150222163924) do
+
+  create_table "authorizations", force: :cascade do |t|
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
+    t.integer  "user_id"
+    t.string   "token"
+    t.string   "secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id"
 
   create_table "groups", force: :cascade do |t|
     t.string   "name",       null: false
@@ -62,9 +74,15 @@ ActiveRecord::Schema.define(version: 20150221161600) do
   add_index "themes", ["group_id"], name: "index_themes_on_group_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                            null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
 end
